9a8f07e9bac28bee830ffc1747e5d1b83a000c10
Sudharshan G <sudarshan.g@healthsignz.com>
2024-06-19 20:18:18 +0530

    commit2

--- HydrationTracker/HydrationTracker/AppDelegate.swift
+++ HydrationTracker/HydrationTracker/AppDelegate.swift
@@ -115,9 +115,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let container = NSPersistentContainer(name: "HydrationTracker")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
                 
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
